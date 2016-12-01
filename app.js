6964e95b6d4315efc0fe3d4f89474a1318e5b8f8
Chase Schell <chase.a.schell@gmail.com>
2016-11-30 22:58:22 -0500

    fixed error with line breaks

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const request = require('request');
 var fs = require('fs');
 
-var token = 'groupme token';
+var token = 'token';
 var stream = fs.createWriteStream("messages.txt");
-var group_ID = 'group 1d#'
+var group_ID = 'group ID'
 
 //var before_id = NULL;
 
@@ -16,30 +16,32 @@ grabMessages(0);
 
 function writeData(parse)
 {
-	for (var i = 0; i < parse.response.messages.length; i++)
+	var messageNum = parse.response.messages.length;
+	for (var i = 0; i < messageNum; i++)
 	{
 		if (parse.response.messages[i].text != null)
 		{
-			var words = parse.response.messages[i].text.split(" ");
+			message = parse.response.messages[i].text.replace(/\r?\n|\r/g," ");
+			var words = message.split(" ");
 			for (var j = 0; j < words.length; j++)
 			{
+				words[j] = words[j].replace(/[?">@.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+				words[j] = words[j].toLowerCase();
 				if (words[j] != "")
 				{
-					words[j] = words[j].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-					words[j] = words[j].toLowerCase();
 					console.log(words[j]);
 					stream.write(parse.response.messages[i].user_id + "," + words[j] + '\n');
 				}
 			}
 		}
 	}
-	if (counter < 50)
+	if (counter < 500)
 	{
 		counter++;
-		console.log(parse.response.messages[19].id);
-		console.log(parse.response.messages.length);
-		console.log(parse.response.messages[19].text);
-		grabMessages(parse.response.messages[19].id);
+		//console.log(parse.response.messages.length);
+		//console.log(messageNum-1);
+		//console.log(parse.response.messages[messageNum-1].text);
+		grabMessages(parse.response.messages[messageNum-1].id);
 	}
 	else
 	{
@@ -59,13 +61,14 @@ function grabMessages (message_id)
 		{
 			messageurl = 'https://api.groupme.com/v3/groups/'+ group_ID + '/messages?token=' + token;
 		}
-	console.log(pack.before_id);
+	//console.log(pack.before_id);
 	request({
 		url: messageurl,
 		method: 'GET',
 		body: JSON.stringify(pack)
 	}, (error, response, body) => {
 		var parse = JSON.parse(body);
+		//console.log(body);
 		writeData(parse);
 	});	
 }
